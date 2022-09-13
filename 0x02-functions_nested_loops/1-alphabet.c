373f4363197c6847ce8174e3557e35451426d3fa
rania <raniazebur2@gmail.com>
2022-09-13 06:53:37 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
