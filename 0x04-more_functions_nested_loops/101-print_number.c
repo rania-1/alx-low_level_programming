40d4bf8ee460a12f7963ac7b0e385c418060afdc
rania <raniazebur2@gmail.com>
2022-09-15 20:12:18 -0700

    Function

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,14 +9,14 @@ void print_number(int n)
 	unsigned int n1;
 
 	if (n < 0)
-	{	
+	{
 		n1 = -n;
 		_putchar('-');
 	} else
 	{
 		n1 = n;
-	}					
-	
+	}
+
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
